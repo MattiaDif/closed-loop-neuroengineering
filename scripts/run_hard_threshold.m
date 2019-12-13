05e655474af160aedaf9de89b558345508e45026
StefanoBuccelli <stefano.buccelli@gmail.com>
2019-12-13 17:09:04 +0100

    multichannel hard threshold not yet analyzed correctly

--- scripts/run_hard_threshold.m
+++ scripts/run_hard_threshold.m
@@ -61,25 +61,25 @@ legend({'simulations','ground truth'})
 %% check matched spikes 
 fs=32e3; % WARNING
 tol=round(0.0005*fs); %tolerance for two spikes to be matched
-timestamps_per_sim=cell(numSims,1);
+match_timestamps_per_sim=cell(numSims,1);
 for curr_sim = 1:numSims
-        timestamps_per_sim{curr_sim,1}=find(ts(curr_sim).Data);
+        match_timestamps_per_sim{curr_sim,1}=find(ts(curr_sim).Data);
 end
 ts_ground_truth=find(spike_train(2,:));
-matched_spikes=zeros(numSims,1);
+n_matched_spikes=zeros(numSims,1);
 for curr_sim = 1:numSims
-    ts_curr_sim=timestamps_per_sim{curr_sim,1};
+    ts_curr_sim=match_timestamps_per_sim{curr_sim,1};
     n_spikes_curr_sim=length(ts_curr_sim);
     for curr_spike=1:n_spikes_curr_sim
         if min(abs(ts_curr_sim(curr_spike)-ts_ground_truth)) <= tol
-            matched_spikes(curr_sim,1)=matched_spikes(curr_sim,1)+1;
+            n_matched_spikes(curr_sim,1)=n_matched_spikes(curr_sim,1)+1;
         end
     end
 end
 %% plot matched spikes (true positive) problem is that there's no 
 %% refractory and thus it's possible for 2 edges to be so close they are counted twice as matched spikes
 figure
-plot(th_sweep,matched_spikes)
+plot(th_sweep,n_matched_spikes)
 hold on
 plot([th_sweep(1) th_sweep(end)],[n_ground_truth n_ground_truth],'-.')
 xlabel('threshold')
@@ -88,19 +88,52 @@ title('matched spikes per threshold')
 legend({'simulations','ground truth'})
 
 
-%% get spikes above refractory period
-ts=timeseries;
+%% get spikes above refractory period (true positives)
+ts_ref=timeseries;
 n_above_refractory=zeros(numSims,1);
 for curr_sim = 1:numSims
         simOut = out(curr_sim);
-        ts(curr_sim,:) = simOut.logsout.get('spike_above_refractory').Values;
-        n_above_refractory(curr_sim,1)=sum(ts(curr_sim,:).Data);
+        ts_ref(curr_sim,:) = simOut.logsout.get('spike_above_refractory').Values;
+        n_above_refractory(curr_sim,1)=sum(ts_ref(curr_sim,:).Data);
+end
+match_ref_timestamps_per_sim=cell(numSims,1);
+for curr_sim = 1:numSims
+        match_ref_timestamps_per_sim{curr_sim,1}=find(ts_ref(curr_sim).Data);
+end
+ts_ground_truth=find(spike_train(2,:));
+n_false_positive=zeros(numSims,1);
+n_matched_ref_spikes=zeros(numSims,1);
+for curr_sim = 1:numSims
+    ts_curr_sim=match_ref_timestamps_per_sim{curr_sim,1};
+    n_spikes_curr_sim=length(ts_curr_sim);
+    for curr_spike=1:n_spikes_curr_sim
+        if min(abs(ts_curr_sim(curr_spike)-ts_ground_truth)) <= tol
+            n_matched_ref_spikes(curr_sim,1)=n_matched_ref_spikes(curr_sim,1)+1;
+        else
+            n_false_positive(curr_sim,1)=n_false_positive(curr_sim,1)+1;
+        end
+    end
 end
 figure
-plot(th_sweep,n_above_refractory)
+subplot(1,2,1)
+plot(th_sweep,n_matched_ref_spikes)
 hold on
 plot([th_sweep(1) th_sweep(end)],[n_ground_truth n_ground_truth],'-.')
 xlabel('threshold')
-ylabel('# of matched spikes')
+ylabel('# True positives')
 title('matched spikes (above refractory period) per threshold')
-legend({'simulations','ground truth'})
+legend({'simulations','ground truth'},'Location','Best')
+ylim([-1 n_ground_truth+5])
+subplot(1,2,2)
+plot(th_sweep,n_false_positive)
+title('False positive')
+xlabel('threshold')
+ylabel('# False positives')
+curr_lim=ylim;
+ylim([-10 curr_lim(2)])
+
+
+
+
+
+
