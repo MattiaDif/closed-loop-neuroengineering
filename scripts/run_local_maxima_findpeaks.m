2f814f8f09ec8c829abad19d07f72480b963c0e1
StefanoBuccelli <stefano.buccelli@gmail.com>
2020-01-20 18:28:49 +0100

    local maxima works as expected even if a bit different from findpeaks (fine)

--- scripts/run_local_maxima_findpeaks.m
+++ scripts/run_local_maxima_findpeaks.m
@@ -13,10 +13,10 @@ cd('models')
 
 %% running a parallel simulation with different thresholds
 % th_sweep=-1:-1:-100; %% sweeping  thresholds
-th_sweep=-1; %% sweeping  thresholds
+th_sweep=-20; %% sweeping  thresholds
 numSims = length(th_sweep);
 
-mdl='local_maxima_findpeaks';
+mdl='local_maxima_findpeaks_2';
 load_system(mdl);
 BlockPaths = find_system(mdl,'Type','Block')
 BlockDialogParameters = get_param([mdl '/Compare_to_threshold'],'DialogParameters')
@@ -33,11 +33,15 @@ n_ground_truth=sum(spike_train(2,:));
 %% get spikes above refractory period (true positives)
 ts_ref=timeseries;
 above_th_ref=timeseries;
+above_th_and_LMM=timeseries;
+LMM=timeseries;
 n_above_refractory=zeros(numSims,1);
 for curr_sim = 1:numSims
         simOut = out(curr_sim);
         ts_ref(curr_sim,:) = simOut.logsout.get('spike_above_refractory').Values;
         above_th_ref(curr_sim,:) = simOut.logsout.get('above_th').Values;
+        above_th_and_LMM(curr_sim,:) = simOut.logsout.get('above_th_and_LMM').Values;
+        LMM(curr_sim,:) = simOut.logsout.get('LMM').Values;
         n_above_refractory(curr_sim,1)=sum(ts_ref(curr_sim,:).Data);
 end
 
@@ -86,4 +90,26 @@ hold on
 plot(loc_6+28,rec_single_ch(2,loc_6+28),'r*')
 plot(loc-2,rec_single_ch(2,loc-2),'bo')
 
+%%
+loc_th_simulink=find(ts_ref.Data>0);
+spikes_from_sim=length(loc_th_simulink);
+[pk_th,loc_th_findpeaks]=findpeaks(double(-rec_single_ch(2,:)),'MinPeakHeight',-th_sweep,'MinPeakDistance',32); 
+spikes_from_findpeaks=length(pk_th);
 
+%% local minimum found.. shifted by 2 (1 for the algorithm, 1 for timeseries shift)
+figure
+plot(rec_single_ch(1,:),rec_single_ch(2,:))
+hold on
+% plot(rec_single_ch(1,find(LMM.Data>0)-2),rec_single_ch(2,find(LMM.Data>0)-2),'r*')
+% plot(rec_single_ch(1,find(above_th_and_LMM.Data>0)-2),rec_single_ch(2,find(above_th_and_LMM.Data>0)-2),'bo')
+plot(rec_single_ch(1,find(ts_ref.Data>0)-2),rec_single_ch(2,find(ts_ref.Data>0)-2)+2,'g^')
+plot(rec_single_ch(1,loc_th_findpeaks),rec_single_ch(2,loc_th_findpeaks)+1,'y<')
+%% difference simulink-findpeaks in samples (after subtracting 2)
+figure
+plot((loc_th_simulink-2)-loc_th_findpeaks')
+
+% % From findpeaks, line 860 (R2019b)
+% % function idx = findPeaksSeparatedByMoreThanMinPeakDistance(y,x,iPk,Pd)
+% % % Start with the larger peaks to make sure we don't accidentally keep a
+% % % small peak and remove a large peak in its neighborhood. 
+% % There’s a sorting of peakValues. This can’t be reproduced. That’s why is different!
