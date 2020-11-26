9063611109502ce86bc39edaaf8217866e5b6895
diflo <diflo@DESKTOP-GEQ2DJV>
2020-11-26 11:13:51 +0100

    Mattia folder

--- Mattia/Model/slprj/raccel/HardThreshold/HardThreshold_data.c
+++
@@ -1,5 +0,0 @@
-#include "HardThreshold.h"
-#include "HardThreshold_private.h"
-P rtP = { 1.0 , - 250.0 , 0.00027 , 4294967295U , 0.0 , 0.0 ,
-3.3333333333333335E-5 , 0.0 , ( 0LL ) , ( 4294967295ULL ) , ( 4294967296ULL )
-, ( 4294967296ULL ) , 1U , 1U , 1U , 1U , 0U , 1 , 0 , 1 , 1 , 1 , 0 } ;
--- Mattia/Model/slprj/raccel/HardThreshold/rtmodel.h
+++
@@ -1,5 +0,0 @@
-#ifndef RTW_HEADER_rtmodel_h_
-#define RTW_HEADER_rtmodel_h_
-#include "HardThreshold.h"
-#define GRTINTERFACE 1
-#endif
