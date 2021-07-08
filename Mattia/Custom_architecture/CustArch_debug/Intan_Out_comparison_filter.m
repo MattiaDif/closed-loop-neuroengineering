fc31f3fcbc82fc6f2672217cf4d449f4ab7c433f
Mattia <di.florio.mattia@gmail.com>
2021-07-08 15:32:38 +0200

    automatic comparison between filter output (INTAN vs Simulink)

--- Mattia/Custom_architecture/CustArch_debug/Intan_Out_comparison_filter.m
+++ Mattia/Custom_architecture/CustArch_debug/Intan_Out_comparison_filter.m
@@ -4,6 +4,19 @@ clc
 
 
 
+%% Change current folder
+if(~isdeployed)
+    cd(fileparts(which(mfilename)));
+end
+curr_folder = pwd;
+path_index = strfind(curr_folder,'closed-loop-neuroscience');
+
+project_path = curr_folder(1:path_index+length('closed-loop-neuroscience')-1);
+addpath(genpath(project_path)); %adding to the Matlab path all the project folder including all the subfolders
+
+
+
+%% INTAN STUFF
 %% Loading file
 [file, path, filterindex] = ...
     uigetfile('*.rhs', 'Select an RHS2000 Data File', 'MultiSelect', 'off');
@@ -29,49 +42,6 @@ dc_data_cust_uint16 = (dc_data_cust/(-19.23*10^-3))+512;
 
 time = t;
 
-% ac_data_orig_uint16 = (ac_data_orig/(0.195*10^-6))+32768;
-% dc_data_orig_uint16 = (dc_data_orig/(-19.23*10^-3))+512;
-
-
-
-%% Load simulation
-% mdl_name = ["Filter_",;
-
-
-
-
-for i = 1:size(ac_data_cust,1)
-    
-    figure
-%     ax(1)=subplot(3,1,1);
-%     stairs(time,ac_data_cust_uint16(i,:)*0.195,'r','LineWidth',1)
-%     title(['Raw data - ch: ',num2str(i)])
-%     ylabel('µV')
-%     set(gca,'FontSize',14)
-%     
-%     ax(2)=subplot(3,1,2);
-%     stairs(time,dc_data_cust_uint16(i,:)*0.195,'b','LineWidth',1)
-%     title(['Filtered data - ch: ',num2str(i)])
-%     ylabel('µV')
-%     set(gca,'FontSize',14)
-    
-%     ax(3)=subplot(3,1,3);
-    stairs(time*1000,(ac_data_cust_uint16(i,:)-32768)*0.195,'r','LineWidth',1),hold on
-    stairs(time*1000,(dc_data_cust_uint16(i,:)-32768)*0.195,'k','LineWidth',1),hold on
-    title(['FPGA Raw vs Filtered signal (in vivo) - ch: ',num2str(i)])
-    xlabel('Time (ms)')
-    ylabel('µV')
-    set(gca,'FontSize',14)
-    legend('Raw','Filtered')
-    
-%     linkaxes(ax,'x')
-    hold off
-    grid on
-    grid minor
-    
-end
-
-
 w = hamming(128);
 noverlap = length(w)/2;
 NFFT = 1024;
@@ -89,7 +59,89 @@ AC_filtered_double = double(dc_data_cust_uint16);
 
 figure
 plot(f,pxx/max(pxx),'r'),hold on
-plot(f_f,pxx_f/max(pxx_f),'b')
+plot(f_f,pxx_f/max(pxx),'b')
 hold off
 title(['Normalized PSD - ch: ',num2str(power_spect_ch)])
 legend('Raw','Filtered')
+
+
+
+%% SIMULINK STUFF
+%% Simulation parameters
+mdl_name = "fixed_point_filter_CustArch_v9";
+sim_type = 'rapid'; %simulation speed
+sim_stop_time = num2str(size(ac_data_cust,2)/fs);   %s
+mdl=convertStringsToChars(mdl_name);
+load_system(mdl);
+set_param(mdl, 'SimulationMode', sim_type)
+set_param(mdl,'StartTime','0','StopTime',sim_stop_time)
+flag_progress = 'off';  %show simulation progress or not
+
+%run simulation
+for curr_sim = 1:size(ac_data_cust,1)
+    
+    signal.signal = [time' ac_data_cust(curr_sim,:)'];
+    in = Simulink.SimulationInput(mdl);
+    
+    out = sim(in,'ShowProgress', flag_progress);
+    
+    ac_data_cust_uint16_sim(curr_sim,:) = double(out.logsout{1}.Values.Data);
+    ac_data_cust_uint16_sim = double(ac_data_cust_uint16_sim);
+    dc_data_cust_uint16_sim(curr_sim,:) = double(out.logsout{2}.Values.Data);
+    dc_data_cust_uint16_sim = double(dc_data_cust_uint16_sim);
+    
+    residuals = dc_data_cust_uint16(curr_sim,:)-dc_data_cust_uint16_sim(curr_sim,:);
+    
+    figure
+    ax(1)=subplot(2,2,1);
+    stairs(time*1000,(ac_data_cust_uint16_sim(curr_sim,:)),'r','LineWidth',1),hold on
+    stairs(time*1000,(dc_data_cust_uint16_sim(curr_sim,:)),'b','LineWidth',1),hold on
+    title(['Simulink Raw vs Filtered signal - ch: ',num2str(curr_sim)])
+%     xlabel('Time (ms)')
+    ylabel('Amplitude (uint16)')
+    set(gca,'FontSize',14)
+    legend('Raw','Filtered')
+    hold off
+    grid on
+    grid minor
+    
+    ax(2)=subplot(2,2,3);
+    stairs(time*1000,(ac_data_cust_uint16(curr_sim,:)),'r','LineWidth',1),hold on
+    stairs(time*1000,(dc_data_cust_uint16(curr_sim,:)),'k','LineWidth',1),hold on
+    title(['FPGA Raw vs Filtered signal - ch: ',num2str(curr_sim)])
+    xlabel('Time (ms)')
+    ylabel('Amplitude (uint16)')
+    set(gca,'FontSize',14)
+    legend('Raw','Filtered')
+    hold off
+    grid on
+    grid minor
+    
+    ax(3)=subplot(2,2,2);
+    stairs(time*1000,(dc_data_cust_uint16_sim(curr_sim,:)),'b','LineWidth',1),hold on
+    stairs(time*1000,(dc_data_cust_uint16(curr_sim,:)),'k','LineWidth',1),hold on
+    title(['Simulink Filtered vs FPGA Filtered signal - ch: ',num2str(curr_sim)])
+%     xlabel('Time (ms)')
+%     ylabel('Amplitude (uint16)')
+    set(gca,'FontSize',14)
+    legend('Simulink','FPGA')
+    hold off
+    grid on
+    grid minor
+    
+    ax(4)=subplot(2,2,4);
+    stairs(time*1000,residuals,'g','LineWidth',1)
+    title(['Residuals (FPGA - Simulink outputs) - ch: ',num2str(curr_sim)])
+    xlabel('Time (ms)')
+%     ylabel('Amplitude (uint16)')
+    set(gca,'FontSize',14)
+    legend('Simulink','FPGA')
+    hold off
+    grid on
+    grid minor
+    linkaxes(ax,'x')
+    
+end
+
+    
+
