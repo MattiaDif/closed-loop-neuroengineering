8480c743b79b9804de6b62086eb252ada6fd7bd6
Mattia <di.florio.mattia@gmail.com>
2021-09-22 22:42:04 +0200

    config file generation

---
+++ Mattia/Custom_architecture/Config_File_generation/Hard_Threshold_Local_Maxima.m
@@ -0,0 +1,33 @@
+clear all
+clc
+
+n_ch = 128;
+
+rng(0,'twister');
+
+a = 24000;
+b = 35000;
+r = (b-a).*rand(n_ch,1) + a;
+
+r_range = [min(r) max(r)]
+
+refractory = 300;    %samples
+
+config_file = zeros(n_ch,1);
+for i=1 : 1 : n_ch
+    
+    config_file((i-1)*2+1) = i-1;
+    config_file((i)*2) = r(i);
+    
+end
+
+config_file = uint16([config_file; n_ch; refractory]);
+
+
+config_file_txt = fopen(['config_file.txt'], 'w');
+
+for i = 1:length(config_file)
+
+    fprintf(config_file_txt,'%d \n',config_file(i));
+
+end
