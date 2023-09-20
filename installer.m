f6a1ddeb96a7f841d39fc5407e32e248a6af2b30
mattiadif <di.florio.mattia@gmail.com>
2023-09-21 00:15:19 +0200

    minor bug solved

--- installer.m
+++ installer.m
@@ -3,7 +3,7 @@ if(~isdeployed)
     cd(fileparts(which(mfilename)));
 end
 curr_folder = pwd;
-path_index = strfind(curr_folder,'closed-loop-neuroscience');
+%path_index = strfind(curr_folder,'closed-loop-neuroengineering');
 
-project_path = curr_folder(1:path_index+length('closed-loop-neuroscience')-1);
-addpath(genpath(project_path)); %adding to the Matlab path all the project folder including all the subfolders
+%project_path = curr_folder(1:path_index+length('closed-loop-neuroengineering')-1);
+addpath(genpath(curr_folder)); %adding to the Matlab path all the project folder including all the subfolders
